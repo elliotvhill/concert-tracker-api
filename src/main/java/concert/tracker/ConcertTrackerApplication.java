430ed5a2ef671306484d4aaa9c4b5306eaac3b90
Elliot <ellthrowaway2@icloud.com>
2025-03-13 20:52:43 -0400

    created application class
    created application.yaml
    
    updated gitignore
    
    created application class and yaml config

---
+++ src/main/java/concert/tracker/ConcertTrackerApplication.java
@@ -0,0 +1,19 @@
+/**
+ * @author elliothill
+ */
+package concert.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTrackerApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertTrackerApplication.class);
+	}
+
+}
