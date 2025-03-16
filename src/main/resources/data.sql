ce7530217c579e65e88435dcf0f7e0ea4cb794f4
Elliot <ellthrowaway2@icloud.com>
2025-03-16 14:03:59 -0400

    created sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO venue (name, street_address, city, state, zip) VALUES ('The Met Philadelphia', '858 N Broad St', 'Philadelphia', 'PA', '19130');
+INSERT INTO venue (name, street_address, city, state, zip) VALUES ('Union Transfer', '1026 Spring Garden St', 'Philadelphia', 'PA', '19123');
+INSERT INTO venue (name, street_address, city, state, zip) VALUES ('The Fillmore', '29 E Allen St', 'Philadelphia', 'PA', '19123');
+INSERT INTO venue (name, street_address, city, state, zip) VALUES ('9:30 Club', '815 V St NW', 'Washington', 'D.C.', '20001');
+INSERT INTO venue (name, street_address, city, state, zip) VALUES ('College Street Music Hall', '238 College St', 'New Haven', 'CT', '06510');
