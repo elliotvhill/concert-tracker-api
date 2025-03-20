28709384f577890b455e524c89e6236e8c8b77ce
Elliot <ellthrowaway2@icloud.com>
2025-03-19 21:30:59 -0400

    fix: added missing interface methods to find artists and concerts by ID in their respective Sets

--- src/main/java/concert/tracker/dao/ArtistDao.java
+++ src/main/java/concert/tracker/dao/ArtistDao.java
@@ -1,9 +1,12 @@
 package concert.tracker.dao;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import concert.tracker.controller.model.ArtistData;
 import concert.tracker.entity.Artist;
 
 public interface ArtistDao extends JpaRepository<Artist, Long> {
-
+	Set<Artist> findAllByArtistIdIn(Set<ArtistData> artists);
 }
--- src/main/java/concert/tracker/dao/ConcertDao.java
+++ src/main/java/concert/tracker/dao/ConcertDao.java
@@ -1,9 +1,12 @@
 package concert.tracker.dao;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import concert.tracker.controller.model.ConcertData;
 import concert.tracker.entity.Concert;
 
 public interface ConcertDao extends JpaRepository<Concert, Long> {
-
+	Set<Concert> findAllByConcertIdIn(Set<ConcertData> concerts);
 }
