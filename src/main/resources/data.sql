9f6fac5d6e5427ca21a392e0d0be693a3e70439c
Elliot <ellthrowaway2@icloud.com>
2025-03-19 21:40:32 -0400

    chore: added addtl data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,7 @@ INSERT INTO venue (name, street_address, city, state, zip) VALUES ('Union Transf
 INSERT INTO venue (name, street_address, city, state, zip) VALUES ('The Fillmore', '29 E Allen St', 'Philadelphia', 'PA', '19123');
 INSERT INTO venue (name, street_address, city, state, zip) VALUES ('9:30 Club', '815 V St NW', 'Washington', 'D.C.', '20001');
 INSERT INTO venue (name, street_address, city, state, zip) VALUES ('College Street Music Hall', '238 College St', 'New Haven', 'CT', '06510');
+
+INSERT INTO artist (name, genre) VALUES ('The National', 'alternative rock');
+INSERT INTO artist (name, genre) VALUES ('Spoon', 'alternative rock');
+INSERT INTO artist (name, genre) VALUES ('Pink', 'pop');
