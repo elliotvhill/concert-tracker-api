689b57f614fd628d6c5f1eaaa59aa959f8942262
Elliot <ellthrowaway2@icloud.com>
2025-03-20 21:38:16 -0400

    changed findAllByArtistIdIn method to take a Set of Long, i.e. the artist IDs, to avoid recursion issues in the DTO class

--- src/main/java/concert/tracker/dao/ArtistDao.java
+++ src/main/java/concert/tracker/dao/ArtistDao.java
@@ -4,9 +4,8 @@ import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import concert.tracker.controller.model.ArtistData;
 import concert.tracker.entity.Artist;
 
 public interface ArtistDao extends JpaRepository<Artist, Long> {
-	Set<Artist> findAllByArtistIdIn(Set<ArtistData> artists);
+	Set<Artist> findAllByArtistIdIn(Set<Long> artistIds);
 }
